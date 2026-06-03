d067ff36e8850301e963ab3dbed0e8470cbe0b64
sevenalpharesearch-debug <sevenalpharesearch@gmail.com>
2026-06-03 17:15:52 +0530

    EQD 03-06-2026

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -193,14 +193,14 @@ export default function Login() {
 
           <form onSubmit={handleLogin}>
             <div className="form-group">
-              <label>Email Address *</label>
+              <label>Login ID *</label>
               <div className="input-box">
                 <span>
                   <svg style={{ width: '20px', height: '20px' }} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
                 </span>
                 <input 
                   type="text" 
-                  placeholder="Enter your email" 
+                  placeholder="Enter your Login ID" 
                   required 
                   value={id}
                   onChange={(e) => {
